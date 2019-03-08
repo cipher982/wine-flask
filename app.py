67dabacfee75ec5fb624b7e4b375ef4401cfa784
David Rose <david010@gmail.com>
2019-03-08 17:18:00 -0500

    added code for CSV reading

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+import random
+import pandas as pd
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
 	return 'Hello, World!'
+
+
+app = Flask(__name__)
+@app.route('/wine')
+def main():
+	df = pd.read_csv('static/fake_wines.csv', sep='|')
+	wine_ix = random.randint(0,len(df))
+	wine_name = df.iloc[wine_ix,:]['name']
+	wine_description = df.iloc[wine_ix,:]['description']
+	return render_template('index.html', 
+                            w_name = wine_name,
+                            w_description = wine_description)
