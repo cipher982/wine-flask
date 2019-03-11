610f7616ff63373464d8f6ca0f610e8dacf47705
David Rose <david010@gmail.com>
2019-03-10 22:59:21 -0400

    added pricing predictions

--- app.py
+++ app.py
@@ -7,10 +7,12 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/wine')
 def main():
-	df = pd.read_csv('static/fake_wines.csv', sep='|')
+	df = pd.read_csv('static/fakes.csv', sep='|')
 	wine_ix = random.randint(0,len(df))
 	wine_name = df.iloc[wine_ix,:]['name']
 	wine_description = df.iloc[wine_ix,:]['description']
+	wine_price = df.iloc[wine_ix,:]['price']
 	return render_template('index.html', 
                             w_name = wine_name,
-                            w_description = wine_description)
+                            w_description = wine_description,
+							w_price = wine_price)
