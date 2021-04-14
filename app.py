b0073c005c48a0a7eb33ea4e826757163668e2c6
David Rose <david010@gmail.com>
2021-04-13 23:26:24 -0400

    update code for pulling matching labels and desc

--- app.py
+++ app.py
@@ -5,11 +5,33 @@ import sys
 
 from firebase_admin import credentials, firestore, initialize_app
 from flask import Flask, render_template
+from google.cloud import storage
+from google.oauth2 import service_account
 
-# Set paths
-DATASET_DIR = "https://storage.googleapis.com/wine-flask/descriptions/"
-IMAGE_DIR = "https://storage.googleapis.com/wine-flask/labels_on_bottle/"
+# Set constants
+GCLOUD_BUCKET = 'wine-flask'
+LABELS_BLOB = "labels_on_bottle_v2"
+DATASET_DIR = f"https://storage.googleapis.com/{GCLOUD_BUCKET}/descriptions/"
+IMAGE_DIR = f"https://storage.googleapis.com/{GCLOUD_BUCKET}/"
 FIRESTORE_COLLECTION = "gpt2-xl-outputs"
+CAT_2_DICT = {
+    1:"Bordeaux Red Blends",
+    2:"Cabernet Sauvignon",
+    3:"Chardonnay",
+    4:"Merlot",
+    5:"Other Red Blends",
+    6:"Other White Blends",
+    7:"Pinot Gris/Grigio",
+    8:"Pinot Noir",
+    9:"Rhone Red Blends",
+    10:"Riesling",
+    11:"Rosé",
+    12:"Sangiovese",
+    13:"Sauvignon Blanc",
+    14:"Syrah/Shiraz",
+    15:"Zinfandel"
+}
+
 
 # Initialize logger
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -23,47 +45,55 @@ firestore_creds = credentials.Certificate(environ['GOOGLE_APPLICATION_CREDENTIAL
 firestore_app = initialize_app(firestore_creds)
 store = firestore.client()
 
-# Set paths
-dataset_dir = "https://storage.googleapis.com/wine-flask/descriptions/"
-image_dir = "https://storage.googleapis.com/wine-flask/labels_on_bottle_v2/"
+# Initialize google.cloud client
+gcloud_creds = service_account.Credentials.from_service_account_file(environ['GOOGLE_APPLICATION_CREDENTIALS'])
 
-def sample_from_firestore(return_random=True, doc_id=None):
+# Create list of bottles from gcloud blob
+storage_client = storage.Client(credentials=gcloud_creds)
+blobs = storage_client.list_blobs(GCLOUD_BUCKET, prefix=LABELS_BLOB, delimiter=None)
+blobs = [(int(i.name.split('cat_')[1].split('_')[0]), i.name) for i in blobs]
+
+
+def sample_from_firestore(return_random=True, label_cat_2=None, doc_id=None):
     LOG.info("Starting firestore sample")
     if return_random == True:
         random_key = store.collection(FIRESTORE_COLLECTION).document().id
         result = store.collection(FIRESTORE_COLLECTION)\
+            .where('category_2', '==', CAT_2_DICT[label_cat_2])\
             .where('id', '>=', random_key)\
             .limit(1)\
             .get()[0]
     else:
         result = store.collection(FIRESTORE_COLLECTION)\
-            .where('id', '==', doc_id)\
+            .where('category_2', '==', CAT_2_DICT[label_cat_2])\
             .limit(1)\
             .get()[0]   
 
     LOG.info(f"Returning firestore sample: {result}")
     return result.to_dict()
 
+def sample_label_from_gcs():
+    return random.choice(blobs)
+
 
 @app.route('/')
 @app.route('/wine')
 def main():
     LOG.info("Starting request")
+    
+    # Sample a random wine label
+    label_cat_2, label_path = sample_label_from_gcs()
+    image_path = IMAGE_DIR + label_path
+    LOG.info(f"Returning image path: {image_path}")
 
-    wine = sample_from_firestore(return_random=True)
-
+    # Sample random description matching the label category
+    wine = sample_from_firestore(return_random=True, label_cat_2=label_cat_2)
     wine_name = wine['name']
     wine_category_1 = wine['category_1']
     wine_category_2 = wine['category_2']
     wine_origin = wine['origin']
     wine_description = wine['description']
     LOG.info(f"Returning description for: {wine_name}")
-
-    # Sample a random wine bottle
-    random_ix = random.sample(range(0, 622), k=1)[0]
-    random_filename = f"wine_bottle_{random_ix:05d}.png"
-    image_path = image_dir + random_filename
-    LOG.info(f"Returning image path: {image_path}")
 
     return render_template(
         'index.html',
