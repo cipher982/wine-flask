17fce9e3507cdfdf22dbca625ef0a1e6c775103f
David Rose <david010@gmail.com>
2021-06-16 21:28:33 -0400

    format with Black

--- app.py
+++ app.py
@@ -10,27 +10,27 @@ from google.oauth2 import service_account
 from retry import retry
 
 # Set constants
-GCLOUD_BUCKET = 'wine-flask'
+GCLOUD_BUCKET = "wine-flask"
 LABELS_BLOB = "labels_on_bottle_v2/bottle_cat_"
 DATASET_DIR = f"https://storage.googleapis.com/{GCLOUD_BUCKET}/descriptions/"
 IMAGE_DIR = f"https://storage.googleapis.com/{GCLOUD_BUCKET}/"
 FIRESTORE_COLLECTION = "gpt2-xl-outputs"
 CAT_2_DICT = {
-    1:"Bordeaux Red Blends",
-    2:"Cabernet Sauvignon",
-    3:"Chardonnay",
-    4:"Merlot",
-    5:"Other Red Blends",
-    6:"Other White Blends",
-    7:"Pinot Gris/Grigio",
-    8:"Pinot Noir",
-    9:"Rhone Red Blends",
-    10:"Riesling",
-    11:"Rosé",
-    12:"Sangiovese",
-    13:"Sauvignon Blanc",
-    14:"Syrah/Shiraz",
-    15:"Zinfandel"
+    1: "Bordeaux Red Blends",
+    2: "Cabernet Sauvignon",
+    3: "Chardonnay",
+    4: "Merlot",
+    5: "Other Red Blends",
+    6: "Other White Blends",
+    7: "Pinot Gris/Grigio",
+    8: "Pinot Noir",
+    9: "Rhone Red Blends",
+    10: "Riesling",
+    11: "Rosé",
+    12: "Sangiovese",
+    13: "Sauvignon Blanc",
+    14: "Syrah/Shiraz",
+    15: "Zinfandel",
 }
 
 
@@ -42,47 +42,54 @@ LOG = logging.getLogger(__name__)
 app = Flask(__name__)
 
 # Initialize Firestore client
-firestore_creds = credentials.Certificate(environ['GOOGLE_APPLICATION_CREDENTIALS'])
+firestore_creds = credentials.Certificate(environ["GOOGLE_APPLICATION_CREDENTIALS"])
 firestore_app = initialize_app(firestore_creds)
 store = firestore.client()
 
 # Initialize google.cloud client
-gcloud_creds = service_account.Credentials.from_service_account_file(environ['GOOGLE_APPLICATION_CREDENTIALS'])
+gcloud_creds = service_account.Credentials.from_service_account_file(
+    environ["GOOGLE_APPLICATION_CREDENTIALS"]
+)
 
 # Create list of bottles from gcloud blob
 storage_client = storage.Client(credentials=gcloud_creds)
 blobs = storage_client.list_blobs(GCLOUD_BUCKET, prefix=LABELS_BLOB, delimiter=None)
-blobs = [(int(i.name.split('cat_')[1].split('_')[0]), i.name) for i in blobs]
+blobs = [(int(i.name.split("cat_")[1].split("_")[0]), i.name) for i in blobs]
 
 
 def sample_from_firestore(return_random=True, label_cat_2=None, doc_id=None):
     LOG.info("Starting firestore sample")
     if return_random == True:
         random_key = store.collection(FIRESTORE_COLLECTION).document().id
-        result = store.collection(FIRESTORE_COLLECTION)\
-            .where('category_2', '==', CAT_2_DICT[label_cat_2])\
-            .where('id', '>=', random_key)\
-            .limit(1)\
+        result = (
+            store.collection(FIRESTORE_COLLECTION)
+            .where("category_2", "==", CAT_2_DICT[label_cat_2])
+            .where("id", ">=", random_key)
+            .limit(1)
             .get()[0]
+        )
     else:
-        result = store.collection(FIRESTORE_COLLECTION)\
-            .where('category_2', '==', CAT_2_DICT[label_cat_2])\
-            .limit(1)\
-            .get()[0]   
+        result = (
+            store.collection(FIRESTORE_COLLECTION)
+            .where("category_2", "==", CAT_2_DICT[label_cat_2])
+            .limit(1)
+            .get()[0]
+        )
 
     LOG.info(f"Returning firestore sample: {result}")
     return result.to_dict()
+
 
 def sample_label_from_gcs():
     return random.choice(blobs)
 
 
-@app.route('/')
-@app.route('/wine')
+@app.route("/")
+@app.route("/wine")
 @retry((Exception), tries=5, delay=0, backoff=0)
 def main():
     LOG.info("Starting request")
-    
+
     # Sample a random wine label
     label_cat_2, label_path = sample_label_from_gcs()
     image_path = IMAGE_DIR + label_path
@@ -92,19 +99,19 @@ def main():
     wine_description = None
     while wine_description == None:
         wine = sample_from_firestore(return_random=True, label_cat_2=label_cat_2)
-        wine_name = wine['name']
-        wine_category_1 = wine['category_1']
-        wine_category_2 = wine['category_2']
-        wine_origin = wine['origin']
-        wine_description = wine['description']
+        wine_name = wine["name"]
+        wine_category_1 = wine["category_1"]
+        wine_category_2 = wine["category_2"]
+        wine_origin = wine["origin"]
+        wine_description = wine["description"]
     LOG.info(f"Returning description for: {wine_name}")
 
     return render_template(
-        'index.html',
+        "index.html",
         w_name=wine_name,
-        w_category_1 = wine_category_1,
-        w_category_2 = wine_category_2,
-        w_origin = wine_origin,
+        w_category_1=wine_category_1,
+        w_category_2=wine_category_2,
+        w_origin=wine_origin,
         w_description=wine_description,
-        w_image=image_path
+        w_image=image_path,
     )
