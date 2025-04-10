9d6c9d2d420803080f51b2332359ececb8c28c22
drose <david010@gmail.com>
2025-04-10 14:54:08 +0200

    Switch to secure minio

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def get_minio_client():
         settings.minio_endpoint,
         access_key=settings.minio_access_key,
         secret_key=settings.minio_secret_key,
-        secure=False,
+        secure=True,
     )
 
 
